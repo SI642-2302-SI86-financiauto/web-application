35c326e2394cf4ff74234b3c59d8fe6d5ea72fe6
María Alexandra Ahuanari Murayari <U20201A836@UPC.EDU.PE>
2023-10-13 21:14:15 -0500

    feat(credit): added credit vehicle route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {CreditVehicleComponent} from "./credit/pages/credit-vehicle/credit-vehicle.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'credit-vehicle' , component: CreditVehicleComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
